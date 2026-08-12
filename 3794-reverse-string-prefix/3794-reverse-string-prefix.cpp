0dd15b6acfcb59a99c8e22abfd7a6a818ad1831a
Chirag Bhadani <kumarchirag978@gmail.com>
2026-08-12 22:46:16 +0530

    Time: 0 ms (100%), Space: 8.9 MB (19.24%) - LeetHub

---
+++ 3794-reverse-string-prefix/3794-reverse-string-prefix.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string reversePrefix(string s, int k) {
+        int low = 0;
+        int high = k - 1;
+        while(low <= high){
+            swap(s[low], s[high]);
+            low++;
+            high--;
+        }
+        return s;
+    }
+};
