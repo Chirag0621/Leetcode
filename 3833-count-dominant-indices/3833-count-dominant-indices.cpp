23492f2882428d9990245f7918e1ac5956a7726e
Chirag Bhadani <kumarchirag978@gmail.com>
2026-08-13 23:53:51 +0530

    Time: 0 ms (100%), Space: 32.3 MB (51.51%) - LeetHub

---
+++ 3833-count-dominant-indices/3833-count-dominant-indices.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int dominantIndices(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+
+        long long sum = nums[n - 1];
+
+        for (int i = n - 2; i >= 0; i--) {
+            int cnt = n - i - 1;
+
+            if (1LL * nums[i] * cnt > sum) {
+                ans++;
+            }
+
+            sum += nums[i];
+        }
+
+        return ans;
+    }
+};
